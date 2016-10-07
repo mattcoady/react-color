91f1742457c0c6d4f80c31d791cc6dcc442bdc8b
Matt Coady <mattcoady@gmail.com>
2016-10-07 15:25:09 -0700

    WIP

--- src/components/common/Alpha.js
+++ src/components/common/Alpha.js
@@ -3,6 +3,7 @@
 import React from 'react'
 import reactCSS from 'reactcss'
 import shallowCompare from 'react-addons-shallow-compare'
+import _ from 'lodash'
 
 import Checkboard from './Checkboard'
 
